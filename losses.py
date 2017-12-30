0278d77c111c3b1c0eab155e1abb3064564f0bb2
Paul <supercolossal.paul@gmail.com>
2017-12-30 18:42:55 +0800

    tweak losses

--- losses.py
+++ losses.py
@@ -7,32 +7,33 @@ def content_loss(vggTrain, vggRef, weight):
         ref = vggRef.content_layer
         gen = vggTrain.content_layer
         size = tf.size(ref)
-        return weight * tf.reduce_sum(tf.nn.l2_loss(ref-gen)) / tf.to_float(size)
+        return weight * tf.reduce_sum(tf.squared_difference(ref, gen)) / tf.to_float(size)
 
 def style_layer_loss(a_S, a_G):
     m, h, w, c = a_G.get_shape().as_list()
     a_S = tf.reshape(a_S, [m, -1, c])
     a_G = tf.reshape(a_G, [m, -1, c])
-
+    size = tf.size(a_S)
     with tf.variable_scope('gram'):
-        size=h*w*c
         GS = tf.matmul(a_S, a_S, transpose_a=True) / tf.to_float(size)
         GG = tf.matmul(a_G, a_G, transpose_a=True) / tf.to_float(size)
     
-    return tf.reduce_sum(tf.nn.l2_loss(GS-GG)) / tf.to_float(tf.size(GG))
+    # squared frobenius norm of the difference
+    # means, (gg-gs)*(gg-gs) sum of all (element wise difference and squared)
+    return tf.reduce_sum(tf.squared_difference(GS, GG))
 
 def style_loss(sess, input_var, vggTrain, vggRef, style_input, style_weight):
     with tf.variable_scope('style_loss'):
         loss = 0
         ref_styles = sess.run(vggRef.style_layers, feed_dict={input_var:style_input})
-
-        for i in range(len(ref_styles)):
+        num_layers = len(ref_styles)
+        for i in range(num_layers):
             with tf.variable_scope('style_loss_layer_'+str(i)):
                 ref = ref_styles[i]
                 gen = vggTrain.style_layers[i]
-                loss += style_layer_loss(ref, gen)
+                loss += style_layer_loss(ref, gen)/tf.to_float(num_layers)
 
-    return style_weight*(loss/tf.to_float(len(ref_styles)))
+    return style_weight*(loss)
 
 def tv_loss(X, weight):
     with tf.variable_scope('tv_loss'):
